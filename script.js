a71c008c1cd93c4ee7d6660531f0d4f932bdaeec
harshkumar <pate0696@algonquinlive.com>
2020-01-13 15:33:58 -0500

    added index.html and script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+let name = 'Harshkumar'
+
+console.log(`Hello!, my name is ${name}`)
+
+let number = 15
+
+console.log(`The number ${number} multiplied by 2 is ${number * 2}.`)
+
+let result = number * 2
+
+console.log(`The number multiplied by 5 is ${result * 5}.`)
+
+console.log(`The result by division is ${(result * 5)/number}.`)
+
+let finalvalue = (result*5)/number
+
+console.log(`The result minus 7 is ${finalvalue - 7}.`)
